fd887120950b494e39ae1522c41ac143b1456424
M.Murat <mmuratakbiyik@gmail.com>
2019-06-10 20:31:45 +0300

    added apple entity

---
+++ src/main/java/com/tutorial/lamdatutorial/entities/Apple.java
@@ -0,0 +1,34 @@
+package com.tutorial.lamdatutorial.entities;
+
+public class Apple {
+
+	private String colour;
+	private Integer weight;
+
+	public Apple(String colour, Integer weight) {
+		this.colour = colour;
+		this.weight = weight;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public Integer getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Integer weight) {
+		this.weight = weight;
+	}
+
+	@Override
+	public String toString() {
+		return "Apple: colour= " + colour + ",weight= " + weight;
+	}
+
+}
---
+++ src/main/java/com/tutorial/lamdatutorial/predicate/ApplePredicate.java
@@ -0,0 +1,6 @@
+package com.tutorial.lamdatutorial.predicate;
+
+public interface ApplePredicate {
+	public boolean test(Apple apple);
+
+}
